4a3d3827fefa0553bd6ec84dff7f0e6a69bcaa94
Misbah Sehar <misbahsahar09@gmail.com>
2026-06-17 17:55:35 +0500

    switch to postgre render sql  database

--- config/settings.py
+++ config/settings.py
@@ -92,12 +92,12 @@ REST_FRAMEWORK = {
 # Using MySQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'ScriptNova'),
-        'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'root123'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
